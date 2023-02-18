e366dfa42acf95604e42e6f0353d3c917ce54bc0
ranjubhat7 <ranjan@publicissapient.com>
2023-02-18 08:36:46 +0530

    bug fix

--- jwt-auth/server.js
+++ jwt-auth/server.js
@@ -1,47 +1,59 @@
-require('dotenv').config()
-const cors = require('cors')
+require('dotenv').config();
+const cors = require('cors');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
 
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const posts = [
-  {
-    username: 'Kyle',
-    title: 'Post 1'
-  },
-  {
-    username: 'Jim',
-    title: 'Post 2'
-  }
-]
+	{
+		username: 'Kyle',
+		title: 'Post 1',
+	},
+	{
+		username: 'Jim',
+		title: 'Post 2',
+	},
+];
 
-app.get('/posts', authenticateToken, (req, res) => {
-  res.json(posts.filter(post => post.username === req.user.name))
-})
+const getProducts = async () => {
+	axios('http://localhost:3333/products')
+		.then((resp) => {
+			return resp;
+		})
+		.catch((error) => {
+			return error;
+		});
+};
+app.get('/products',authenticateToken, (req, res) => {
+	axios('http://localhost:3333/products')
+		.then((resp) => {
+      // console.log(resp)
+			res.json(resp.data)
+		})
+		.catch((error) => {
+      console.log(error)
+			return res.sendStatus(401)
+		});
+});
 
-// app.post('/login', (req,res) => {
-//     const username =  req.body.username
-//     const user = { name: username }
 
-//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-//     res.json({ accessToken: accessToken })
-// })
 
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+  console.log(req.headers["authorization"])
+	const authHeader = req.headers['authorization'];
+	// const token = authHeader && authHeader.split(' ')[1];
+	if (authHeader == null) return res.sendStatus(403);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
+	jwt.verify(authHeader, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+		console.log(err);
+		if (err) return res.sendStatus(403);
+		req.user = user;
+		next();
+	});
 }
 
 app.listen(3000);
